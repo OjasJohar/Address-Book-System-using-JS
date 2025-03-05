f127f64e44210ed9a06d9fc7856d23d40e44bd14
OjasJohar <ojasjohar@gmail.com>
2025-03-05 16:33:50 +0530

    [Ojas] Add. Implement view contacts by city/state

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 const AddressBook = require('./services/AddressBook');
 const Contact = require('./models/Contact');
 
@@ -7,12 +6,12 @@ const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
-        "Ojas", "Johar", "Narela", "Bhopal", "Madhya Pradesh", "462001", "9876543210", "helloworld@gmail.com"
+        "Ojas", "Johar", "India", "Bhopal", "Madhya Pradesh", "462001", "9876543210", "helloworld@gmail.com"
     );
     console.log(addressBook.addContact(contact1)); 
 
     const contact2 = new Contact(
-         "Ramu", "Patel", "Narela", "Bhopal", "Madhya Pradesh", "462002", "9564873210", "chandalala@gmail.com"
+         "Ramu", "Patel", "India", "Bhopal", "Madhya Pradesh", "462002", "9564873210", "chandalala@gmail.com"
     )
     console.log(addressBook.addContact(contact2)); 
     console.log("All Contacts Before editing", addressBook.getAllContacts());
@@ -23,12 +22,58 @@ try {
     console.log("Total Contacts:", addressBook.countContacts()); 
 
     const duplicateContact = new Contact(
-        "Ojas", "Johar", "Narela", "Bhopal", "Madhya Pradesh", "462033", "9876559210", "helloworldrt@gmail.com"
+        "Ojas", "Johar", "India", "Bhopal", "Madhya Pradesh", "462033", "9876559210", "helloworldrt@gmail.com"
     );
     console.log(addressBook.addContact(duplicateContact));  
 
     console.log("Contacts in India:");
-    console.log(addressBook.searchByCityOrState("Bhopal"));
+    console.log(addressBook.searchByCityOrState("India"));
+    console.log("Viewing Persons by City or State:");
+    console.log(addressBook.viewPersonsByCityOrState());
+
+} catch (error) {
+    console.error("Error:", error.message);
+}
+
+try {
+    const invalidContact = new Contact(
+        "Mani", "Pate", "abcd", "In", "I", "109A01", "123445", "invalid-email"
+    );
+    console.log(addressBook.addContact(invalidContact));
+} catch (error) {
+    console.error("Error:", error.message);
+}const AddressBook = require('./services/AddressBook');
+const Contact = require('./models/Contact');
+
+const addressBook = new AddressBook();
+// console.log(Object.getOwnPropertyNames(AddressBook.prototype));
+
+try {
+    const contact1 = new Contact(
+        "Ojas", "Johar", "India", "Bhopal", "Madhya Pradesh", "462001", "9876543210", "helloworld@gmail.com"
+    );
+    console.log(addressBook.addContact(contact1)); 
+
+    const contact2 = new Contact(
+         "Ramu", "Patel", "India", "Bhopal", "Madhya Pradesh", "462002", "9564873210", "chandalala@gmail.com"
+    )
+    console.log(addressBook.addContact(contact2)); 
+    console.log("All Contacts Before editing", addressBook.getAllContacts());
+    console.log(addressBook.editContact("Ojas", { address: "Raura", city: "New London", phone: "9284567890" }));
+    console.log("All Contacts After editing", addressBook.getAllContacts());
+    console.log(addressBook.deleteContact("Ojas"));
+    console.log("All Contacts After Deleting:", addressBook.getAllContacts());
+    console.log("Total Contacts:", addressBook.countContacts()); 
+
+    const duplicateContact = new Contact(
+        "Ojas", "Johar", "India", "Bhopal", "Madhya Pradesh", "462033", "9876559210", "helloworldrt@gmail.com"
+    );
+    console.log(addressBook.addContact(duplicateContact));  
+
+    console.log("Contacts in India:");
+    console.log(addressBook.searchByCityOrState("India"));
+    console.log("Viewing Persons by City or State:");
+    console.log(addressBook.viewPersonsByCityOrState());
 
 } catch (error) {
     console.error("Error:", error.message);
