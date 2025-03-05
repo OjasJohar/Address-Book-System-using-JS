d4dcd2bbc78b416c3c46f56c9b9401d8dcf1dc0e
OjasJohar <ojasjohar@gmail.com>
2025-03-05 14:44:33 +0530

    [Ojas] Add. Implement contact search and edit functionality

--- src/index.js
+++ src/index.js
@@ -10,17 +10,19 @@ try {
     console.log(addressBook.addContact(contact1)); 
 
     const contact2 = new Contact(
-        "Anush", "Tengu", "Indore", "India","In","1004500","4991188633","helloworldd@gmail.com"
+        "Anush", "Tengu", "Bhopal", "India","In","1000000","9198563558","chandalala@gmail.com"
     )
     console.log(addressBook.addContact(contact2)); 
-    console.log("All Contacts", addressBook.getAllContacts());
+    console.log("All Contacts Before editing", addressBook.getAllContacts());
+    console.log(addressBook.editContact("Sejal", { address: "Mahim", city: "Navi Mumbai", phone: "9234567890" }));
+    console.log("All Contacts After editing", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
 
 try {
     const invalidContact = new Contact(
-        "Man", "pat", "abc", "In", "I", "767A5", "12345", "invalid-email"
+        "oja", "joh", "abc", "Ind", "I", "100A01", "12345", "invalid-email"
     );
     console.log(addressBook.addContact(invalidContact));
 } catch (error) {
