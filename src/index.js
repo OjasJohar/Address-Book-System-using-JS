21bba100d9c81369a8a3ab77c54eab36d61d7879
OjasJohar <ojasjohar@gmail.com>
2025-03-05 15:44:22 +0530

    [Ojas] Add. Added ability to find and delete contact by name

--- src/index.js
+++ src/index.js
@@ -10,19 +10,21 @@ try {
     console.log(addressBook.addContact(contact1)); 
 
     const contact2 = new Contact(
-        "Anush", "Tengu", "Bhopal", "India","In","1000000","9198563558","chandalala@gmail.com"
+        "Anush", "Tenguriya", "Bhopal", "India","In","1000000","9198563558","chandalala@gmail.com"
     )
     console.log(addressBook.addContact(contact2)); 
     console.log("All Contacts Before editing", addressBook.getAllContacts());
-    console.log(addressBook.editContact("Sejal", { address: "Mahim", city: "Navi Mumbai", phone: "9234567890" }));
+    console.log(addressBook.editContact("Rakesh", { address: "Pipariya", city: "New York", phone: "9234567890" }));
     console.log("All Contacts After editing", addressBook.getAllContacts());
+    console.log(addressBook.deleteContact("Rakesh"));
+    console.log("All Contacts After Deleting:", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
 
 try {
     const invalidContact = new Contact(
-        "oja", "joh", "abc", "Ind", "I", "100A01", "12345", "invalid-email"
+        "Ojas", "joh", "abc", "Ind", "I", "100A01", "12345", "invalid-email"
     );
     console.log(addressBook.addContact(invalidContact));
 } catch (error) {
