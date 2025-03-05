8544d2369ed982bb0c76a7ff14455e803e949486
OjasJohar <ojasjohar@gmail.com>
2025-03-05 16:20:48 +0530

    [Ojas] Add. functionality to search contacts by city or state

--- src/index.js
+++ src/index.js
@@ -1,37 +1,42 @@
+
 const AddressBook = require('./services/AddressBook');
 const Contact = require('./models/Contact');
 
 const addressBook = new AddressBook();
+// console.log(Object.getOwnPropertyNames(AddressBook.prototype));
 
 try {
     const contact1 = new Contact(
-        "Ojas", "Johar", "Bhopal", "India", "Madhya Pradesh", "462001", "9876543210", "helloworld@gmail.com"
+        "Ojas", "Johar", "Narela", "Bhopal", "Madhya Pradesh", "462001", "9876543210", "helloworld@gmail.com"
     );
     console.log(addressBook.addContact(contact1)); 
 
     const contact2 = new Contact(
-        "Ramu", "Patel", "Bhopal", "India", "Madhya Pradesh", "462002", "9564873210", "chandalala@gmail.com"
-    );
+         "Ramu", "Patel", "Narela", "Bhopal", "Madhya Pradesh", "462002", "9564873210", "chandalala@gmail.com"
+    )
     console.log(addressBook.addContact(contact2)); 
-    
     console.log("All Contacts Before editing", addressBook.getAllContacts());
-    console.log(addressBook.editContact("Ojas", { address: "Raura", city: "London", phone: "9234567890" }));
+    console.log(addressBook.editContact("Ojas", { address: "Raura", city: "New London", phone: "9284567890" }));
     console.log("All Contacts After editing", addressBook.getAllContacts());
-    console.log(addressBook.deleteContact("Ramu"));
+    console.log(addressBook.deleteContact("Ojas"));
     console.log("All Contacts After Deleting:", addressBook.getAllContacts());
     console.log("Total Contacts:", addressBook.countContacts()); 
 
     const duplicateContact = new Contact(
-        "Ojas", "Johar", "Bhopal", "India", "Madhya Pradesh", "462001", "9876543270", "helloiworld@gmail.com"
+        "Ojas", "Johar", "Narela", "Bhopal", "Madhya Pradesh", "462033", "9876559210", "helloworldrt@gmail.com"
     );
-    console.log(addressBook.addContact(duplicateContact)); 
+    console.log(addressBook.addContact(duplicateContact));  
+
+    console.log("Contacts in India:");
+    console.log(addressBook.searchByCityOrState("Bhopal"));
+
 } catch (error) {
     console.error("Error:", error.message);
 }
 
 try {
     const invalidContact = new Contact(
-        "Ma", "Pa", "Ind", "India", "MP", "100001", "12345", "invalid-email"
+        "Mani", "Pate", "abcd", "In", "I", "109A01", "123445", "invalid-email"
     );
     console.log(addressBook.addContact(invalidContact));
 } catch (error) {
